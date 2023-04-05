175d63bd922d4edbfcf15c8df46e30764e68775c
Yudi <singh.yudi10@gmail.com>
2023-04-06 01:44:56 +0530

    navbar complete

--- src/components/utils/Navbar.js
+++ src/components/utils/Navbar.js
@@ -1,108 +1,117 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import AppsIcon from '@mui/icons-material/Apps';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import { makeStyles } from '@material-ui/core/styles';
-import Menu from '@material-ui/core/Menu'; // Import Menu component
-import MenuItem from '@material-ui/core/MenuItem';
-import Grid from '@material-ui/core/Grid';
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
-import SecondaryNavbar from './SecondaryNavbar';
-import { Divider } from '@material-ui/core';
+import { useState } from "react";
+import * as React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AppsIcon from "@mui/icons-material/Apps";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { makeStyles } from "@material-ui/core/styles";
+import Menu from "@material-ui/core/Menu"; // Import Menu component
+import MenuItem from "@material-ui/core/MenuItem";
+import Grid from "@material-ui/core/Grid";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import SecondaryNavbar from "./SecondaryNavbar";
+import { Divider } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     backgroundColor: theme.palette.primary1.main,
-    boxShadow: 'none',
-},
-    Toolbar: {
-      alignItems: 'center',
-      height: 'auto'
+    boxShadow: "none",
+  },
+  Toolbar: {
+    alignItems: "center",
+    height: "auto",
   },
   title: {
     flexGrow: 1,
-    display: 'flex',
-    justifyContent: 'flex-start'
+    display: "flex",
+    justifyContent: "flex-start",
   },
   dropdownButton: {
     // marginLeft: theme.spacing(1),
     // marginRight: theme.spacing(1),
-    textTransform: 'none',
-    color: 'inherit',
+    textTransform: "none",
+    color: "inherit",
   },
   search: {
-    position: 'relative',
-    borderRadius: '20px',
+    position: "relative",
+    borderRadius: "20px",
     backgroundColor: theme.palette.common.white,
     marginLeft: theme.spacing(2),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
-      width: '60%',
+      width: "60%",
     },
+    justifyContent: "space-between" /* Add this line */,
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.palette.secondary.main
+    height: "100%",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    color: theme.palette.secondary.main,
   },
+
+  //input section
   inputRoot: {
-    color: 'inherit',
+    color: "inherit",
   },
   inputInput: {
-    color: theme.palette.bg.contrastText,
+    color: theme.palette.primary1.main,
+    fontSize: "0.9em",
     // padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
-    // paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '100%',
-      '&:focus': {
-        width: '100%',
+    paddingLeft: `calc(1em + ${theme.spacing(0.2)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "100%",
+      "&:focus": {
+        width: "100%",
       },
+    },
+    "&.MuiInputBase-input": {
+      color: theme.palette.primary1.main,
     },
   },
   userIcon: {
     marginLeft: theme.spacing(1),
-    display:'None',
-    [theme.breakpoints.up('sm')]: {
-        display: 'block'
-      },
+    display: "None",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
   },
   cartIcon: {
     marginLeft: theme.spacing(1),
   },
   dptGrid: {
-    display:'None',
-    [theme.breakpoints.up('sm')]: {
-        display: 'block'
-      },
+    display: "None",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
   },
   logoIcon: {
     color: theme.palette.secondary.main,
-    fontSize: 'large'
+    fontSize: "large",
   },
   divider: {
     backgroundColor: theme.palette.primary2.main,
     height: 1,
-    width: '100%'
+    width: "100%",
   },
 }));
 
-const Navbar = ( { categories } ) => {
+const Navbar = ({ categories }) => {
   const classes = useStyles();
+  const [searchValue, setSearchValue] = useState("");
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleMenuOpen = (event) => {
@@ -113,67 +122,82 @@ const Navbar = ( { categories } ) => {
     setAnchorEl(null);
   };
 
-  const renderMenuItems = ( catgs ) => {
+  const renderMenuItems = (catgs) => {
     return (
-        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>        
-            {catgs.map((cat, index) => {
-                return <MenuItem key={index} onClick={handleMenuClose}>{cat}</MenuItem>
-            })}
-        </Menu>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        {catgs.map((cat, index) => {
+          return (
+            <MenuItem key={index} onClick={handleMenuClose}>
+              {cat}
+            </MenuItem>
+          );
+        })}
+      </Menu>
     );
   };
-
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+    console.log("Search value:", event.target.value);
+    // Do something with the search value, such as making an API request
+  };
   return (
     <div>
-        <AppBar position="static" className={classes.appBar}>
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar className={classes.Toolbar}>
-            {/* Logo */}
-            <Grid className={classes.title}>
-                <Typography variant="h6" >
-                    QuickBuy
-                </Typography>
-                <KeyboardDoubleArrowRightIcon className={classes.logoIcon}/>
-            </Grid>
-            {/* Dropdown Button */}
-            <Grid className={classes.dptGrid}>
-                <IconButton
-                className={classes.dropdownButton}
-                color="inherit"
-                onClick={handleMenuOpen}
-                >
-                <AppsIcon />
-                <Typography variant="body2" style={{fontWeight: 'bold'}}>
-                    Departments
-                </Typography>
-                </IconButton>
-            </Grid>
-            {renderMenuItems(categories)}
-            
-            {/* Search Bar */}
-            <div className={classes.search}>
-            <div className={classes.searchIcon}>
-                <SearchIcon />
-            </div>
+          {/* Logo */}
+          <Grid className={classes.title}>
+            <Typography variant="h6">QuickBuy</Typography>
+            <KeyboardDoubleArrowRightIcon className={classes.logoIcon} />
+          </Grid>
+          {/* Dropdown Button */}
+          <Grid className={classes.dptGrid}>
+            <IconButton
+              className={classes.dropdownButton}
+              color="inherit"
+              onClick={handleMenuOpen}
+            >
+              <AppsIcon />
+              <Typography
+                variant="body2"
+                style={{ fontWeight: "bold", marginLeft: "2px" }}
+              >
+                Departments
+              </Typography>
+            </IconButton>
+          </Grid>
+          {renderMenuItems(categories)}
+          {/* Search Bar */}
+          <div className={classes.search}>
             <InputBase
-                placeholder="Search for items of QuickBuy store here"
-                classes={{
+              value={searchValue}
+              onChange={handleSearchChange}
+              style={{ flex: 1 }}
+              placeholder="Search for items of QuickBuy store here"
+              classes={{
                 root: classes.inputRoot,
                 input: classes.inputInput,
-                }}
+              }}
             />
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-            {/* User Icon */}
-            <IconButton className={classes.userIcon} color="inherit">
+          </div>
+          {/* User Icon */}
+          <IconButton className={classes.userIcon} color="inherit">
             <AccountCircleIcon />
-            </IconButton>
-            {/* Cart Icon */}
-            <IconButton className={classes.cartIcon} color="inherit">
+          </IconButton>
+          {/* Cart Icon */}
+          <IconButton className={classes.cartIcon} color="inherit">
             <ShoppingCartIcon />
-            </IconButton>
+          </IconButton>
         </Toolbar>
-        </AppBar>
-        <Divider className={classes.divider} />
-        <SecondaryNavbar categories={categories}/>
+      </AppBar>
+      <Divider className={classes.divider} />
+      <SecondaryNavbar categories={categories} />
     </div>
   );
 };
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,29 +1,28 @@
-
-import { createTheme } from '@material-ui/core/styles'
+import { createTheme } from "@material-ui/core/styles";
 
 const theme = createTheme({
   palette: {
     primary1: {
-      main: '#127CDF', 
-      contrastText: '#ffffff', 
+      main: "#127CDF",
+      contrastText: "#ffffff",
     },
     primary2: {
-      main: '#499DE6', 
-      contrastText: '#ffffff', 
+      main: "#499DE6",
+      contrastText: "#ababab",
     },
-    
+
     primary3: {
-      main: '#76B9EF', 
-      contrastText: '#ffffff', 
+      main: "#76B9EF",
+      contrastText: "#ffffff",
     },
     secondary: {
-      main: '#FFD152', 
-      contrastText: '#000000', 
+      main: "#FFD152",
+      contrastText: "#000000",
     },
     bg: {
-        main: '#f5f7fa',
-        contrastText: '#282829', 
-    }
+      main: "#f5f7fa",
+      contrastText: "#282829",
+    },
   },
 });
 
